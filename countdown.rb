f1d7d37788082a6959a1bcce89ea9570cb76a2e9
Ally Morson <ally.morson@gmail.com>
2019-09-20 14:00:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,25 +1,4 @@
-def countdown
-number = 10 
-while number >= 0
-puts "#{number} SECOND(S)"
-number = number - 1
-end 
-puts "HAPPY NEW YEAR"
-end 
-countdown
-
-def countdown_with_sleep
-number = 10 
-while number >= 0
-puts "#{number}"
-number = number - 1
-sleep (0.5) 
-end
-puts "HAPPY NEW YEAR"
-end 
-countdown_with_sleep
-
-def countdown(seconds_to_midnight)
+def countdown(seconds_to_midnight = 10)
    while seconds_to_midnight > 0
      puts "#{seconds_to_midnight} SECOND(S)!"
      seconds_to_midnight -= 1
@@ -27,7 +6,7 @@ def countdown(seconds_to_midnight)
    "HAPPY NEW YEAR!"
  end
  
-def countdown_with_sleep(seconds_to_midnight)
+def countdown_with_sleep(seconds_to_midnight = 10)
    while seconds_to_midnight > 0
      puts "#{seconds_to_midnight} SECOND(S)!"
      sleep(1)
